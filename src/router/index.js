dc5dcd74d16c1b18fd76f172e988342ea65dbf9f
GabrielGmz <alonsogg937@gmail.com>
2025-06-10 15:52:38 -0600

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,14 @@ import recuperacion from '@/views/Recuperacion/recuperacion.vue'
 import Verificacion from '@/views/Verificacion/verificacion.vue'
 import perfil from '@/views/Perfil/perfil.vue'
 import Objetivos from '@/views/Perfil/objetivos.vue'
+import PlanNutricion from '@/views/Perfil/planNutricion.vue'
+import Conocimiento from '@/views/Perfil/Conocimiento.vue'
+import GenerarRutina from '@/views/Perfil/GenerarRutina.vue'
+import PlanCompleto from '@/views/Perfil/PlanCompleto.vue'
+import Entrenamiento from '@/views/Perfil/Entrenamiento.vue'
+import DiaPecho from '@/views/Perfil/DiaPecho.vue'
+import PantallaPrincipal from '@/views/Principal/PantallaPrincipal.vue'
+import Rutinas from '@/views/Principal/Rutinas.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
@@ -14,7 +22,15 @@ const routes = [
   { path: '/verificacion', name: 'VerificacionCorreo', component: Verificacion},
   { path: '/recuperacion', name: 'Recuperacion', component: recuperacion },
   { path: '/perfil', name: 'Perfil', component: perfil },
-  { path: '/objetivos', name: 'Objetivos', component: Objetivos}
+  {path: '/pantallaPrincipal', name: 'PantallaPrincipal', component: PantallaPrincipal},
+  { path: '/rutinas', name: 'Rutinas', component: Rutinas },
+  { path: '/objetivos', name: 'Objetivos', component: Objetivos},
+  { path: '/planNutricion', name: 'planNutricion', component: PlanNutricion},
+  { path: '/Conocimiento', name: 'Conocimiento', component: Conocimiento },
+  { path: '/GenerarRutina', name: 'GenerarRutina', component: GenerarRutina },
+  { path: '/PlanCompleto', name: 'PlanCompleto', component: PlanCompleto },
+  { path: '/Entrenamiento', name: 'Entrenamiento', component: Entrenamiento },
+  { path: '/DiaPecho', name: 'DiaPecho', component: DiaPecho }
 ]
 
 const router = createRouter({
